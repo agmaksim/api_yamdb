0c7d6e83c05ffe565878b5e815103cd68f6aa37d
Normal_One <nlavrov2007@gmail.com>
2021-12-17 15:59:04 +0100

    Pagination and filters

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import get_object_or_404
 from django.db.models import Avg
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
+from rest_framework.pagination import LimitOffsetPagination
 
-from .serializers import CategorySerializer, CommentSerializer, GenreSerializer, ReviewSerializer, TitleReadSerializer, TitleWriteSerializer
+from .serializers import (
+    CategorySerializer,
+    CommentSerializer,
+    GenreSerializer,
+    ReviewSerializer,
+    TitleReadSerializer,
+    TitleWriteSerializer
+)
 from reviews.models import Category, Comment, Genre, Review, Title
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=name',)
 
     def perform_create(self, serializer):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
@@ -20,6 +32,9 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all().annotate(Avg('reviews__score'))
+    pagination_class = LimitOffsetPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('name', 'category__slug', 'genre__slug')
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
@@ -30,10 +45,14 @@ class TitleViewSet(viewsets.ModelViewSet):
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=name',)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
         title_id = self.kwargs.get('title_id')
@@ -50,3 +69,6 @@ class CommentViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=name',)
